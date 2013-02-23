4beca54ea1cc882aa44f0e26b31edc9e6f82b247
Jogga <joachim@froestl.com>
2013-02-23 19:16:51 +0100

    main.js require setup

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,21 @@ requirejs.config({
 	paths: {
 		backbone: 'lib/backbone',
 		underscore: 'lib/underscore',
-		jquery: 'lib/jquery-1.9.1'
+		jquery: 'lib/jquery-1.9.1.min'
+	},
+	shim: {
+		'backbone': {
+			deps: ['underscore'],
+			exports: 'backbone'
+		},
+		'underscore': {
+			exports: '_'
+		}
 	}
+});
+
+requirejs(['backbone', 'jquery'],
+function(backbone, $){
+	// Initialize...
+	console.log('energy!');
 });
