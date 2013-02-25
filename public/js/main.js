6d243b81c02d85e5e3db81778457b086aa1fa226
Jogga <joachim@froestl.com>
2013-02-25 15:16:18 +0100

    fixed require.js shim, modules

--- public/js/main.js
+++ public/js/main.js
@@ -4,22 +4,14 @@ requirejs.config({
 		underscore: 'lib/underscore',
 		jquery: 'lib/jquery-1.9.1.min'
 	},
-//	shim: {
-//		'backbone': {
-//			deps: ['underscore', 'jquery'],
-//			exports: 'backbone'
-//		},
-//		'underscore': {
-//			exports: '_'
-//		},
-//		'jquery': {
-//			exports: '$'
-//		}
-//	}
 	shim: {
-		'app': {
-			deps: ['underscore', 'jquery', 'backbone'],
-			exports: 'App'
+		'backbone': {
+			deps: ['jquery', 'underscore'],
+			exports: 'Backbone'
+		},
+		
+		underscore: {
+			exports: '_'
 		}
 	}
 });
